f14a3cce483c20d75e985dba5daca015bd87a9ac
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-02-25 22:28:00 +0100

    Added hook code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {useState, useEffect} from 'react'
+
+const useViewportVisibility = () => {
+  const [visibilityState, setVisibility] = useState(document.visibilityState)
+
+  const handleVisibilityChange = () => {
+    setVisibility(document.visibilityState)
+  }
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', handleVisibilityChange, true)
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  }, [])
+
+  return visibilityState
+}
+
+export default useViewportVisibility
